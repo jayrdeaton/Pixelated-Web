e03905f90b6e3866dc8f900dff72e85857c66f88
Jay Deaton <jayrdeaton@gmail.com>
2026-06-03 10:53:29 +0700

    updated icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,7 +9,7 @@ export default defineNuxtConfig({
   },
   app: {
     head: {
-      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/favicon.svg' }]
+      link: [{ rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' }]
     }
   },
   css: ['~/assets/css/main.css'],
